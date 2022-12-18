7ff5020914182144c3cfb77849a8013aaf631ecb
byetevinn <stevan14padilha@gmail.com>
2022-12-18 02:13:58 -0300

    fix:add_allowed_hosts

--- bandkamp/settings.py
+++ bandkamp/settings.py
@@ -29,7 +29,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("DEBUG", False)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["bandkampdeploy-production-de31.up.railway.app", "0.0.0.0"]
 
 
 # Application definition
